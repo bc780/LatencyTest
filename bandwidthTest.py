f48d3a2573f0d437d1d4dc8cd95aa5e6c4363057
bc780 <124471008+bc780@users.noreply.github.com>
2024-05-29 00:03:05 -0400

    run code
    send tensors back and forth

---
+++ bandwidthTest.py
@@ -0,0 +1,28 @@
+import torch
+import torch.distributed as dist
+import argparse
+
+def run(world_size, rank):
+    tensor = torch.zeros(1)
+    for i in range(0,rank):
+        #recv data of all ranks before
+        for j in range(8):
+            dist.recv(tensor=tensor,src=i)
+    for i in range(world_size):
+        #send data to all ranks
+        #ensure it does not send to itself
+        if i != rank:
+            for j in range(8):
+            #generate random tensor of correct size
+                tensor = torch.rand(250*(10**j))
+                dist.send(tensor=tensor,dst=i)
+    for i in range(rank+1,world_size):
+        #recv data of all ranks after
+        for j in range(8):
+            dist.recv(tensor=tensor,src=i)
+
+def main():
+    parser = argparse.ArgumentParser()
+
+if __name__ == "__main__":
+    main()
