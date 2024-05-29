4e3ad836076f04d832dc615fc15ea7426e9e0c74
bc780 <124471008+bc780@users.noreply.github.com>
2024-05-29 17:49:24 -0400

    debug script fixed

--- bandwidthTestDebug.sh
+++ bandwidthTestDebug.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-#SBATCH -N 16
+#SBATCH -N 4
 #SBATCH -C gpu
-#SBATCH -G 16
-#SBATCH -q preempt
-#SBATCH -J BandwidthTest
+#SBATCH -G 4
+#SBATCH -q debug
+#SBATCH -J BandwidthTestDebug
 #SBATCH --mail-user=bc780@scarletmail.rutgers.edu
 #SBATCH --mail-type=ALL
-#SBATCH -t 00:10:00
+#SBATCH -t 00:05:00
 #SBATCH -A m4410_g
 
 #OpenMP settings:
+module load python/3.11
+module load pytorch/2.0.1
 export OMP_NUM_THREADS=1
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 export MASTER_ADDR=$(hostname)
 export MASTER_PORT=29500
 
-
 #run the application:
 #applications may perform better with --gpu-bind=none instead of --gpu-bind=single:1 
-srun -n 16 -c 128 --cpu_bind=cores -G 16 --gpu-bind=single:1  bandwidthTest.py
+srun -n 4 -c 128 --cpu_bind=cores -G 4 --gpu-bind=none python bandwidthTest.py
