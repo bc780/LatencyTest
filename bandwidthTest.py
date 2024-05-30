26ffd5e0537cf3688a603ce6d85d2c03179dcbbd
bc780 <124471008+bc780@users.noreply.github.com>
2024-05-29 22:41:00 -0400

    added time logging

--- bandwidthTest.py
+++ bandwidthTest.py
@@ -1,33 +1,51 @@
 import torch
 import torch.distributed as dist
-import argparse
+# import argparse
 import logging
 import os
+import time
 
 from utilsNersc.logging import config_logging
 from utilsNersc.distributed import init_workers
 
+
+# max and base of exponents to modify data size
+maxExp = 8
+base = 4
+
 def run(world_size, rank):
-    tensor = torch.zeros(1)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    tensor = torch.zeros(1).to(device)
     for i in range(0,rank):
         #recv data of all ranks before
-        for j in range(8):
+        logging.info("Accepting from rank %i", i)
+        for j in range(maxExp):
+            start  = time.time()
             dist.recv(tensor=tensor,src=i)
+            logging.info("recv %i -> %i # %i at %f", i, rank, j, start)
     for i in range(world_size):
         #send data to all ranks
         #ensure it does not send to itself
         if i != rank:
-            for j in range(8):
+            logging.info("Sending to rank %i", i)
+            for j in range(maxExp):
             #generate random tensor of correct size
-                tensor = torch.rand(250*(10**j))
+                tensor = torch.rand(250*(base**j)).to(device)
+                start  = time.time()
                 dist.send(tensor=tensor,dst=i)
+                logging.info("sent %i -> %i #%i at %f", rank, i, j, start)
     for i in range(rank+1,world_size):
         #recv data of all ranks after
-        for j in range(8):
+        logging.info("Accepting from rank %i", i)
+        for j in range(maxExp):
+            start = time.time()
             dist.recv(tensor=tensor,src=i)
+            logging.info("recv %i -> %i #%i at %f", i, rank, j, start)
+
+    exit()
 
 def main():
-    parser = argparse.ArgumentParser()
+    # parser = argparse.ArgumentParser()
 
     rank, n_ranks = init_workers("nccl")
 
@@ -41,6 +59,7 @@ def main():
     logging.info('Initialized rank %i out of %i', rank, n_ranks)
 
     run(n_ranks, rank)
+    exit()
 
     
 
