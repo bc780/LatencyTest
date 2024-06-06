e6de0e916d9b1751fad202bbc866f7894edcf235
bc780 <124471008+bc780@users.noreply.github.com>
2024-06-06 00:22:19 -0400

    send recv fixed
    time sync and name grab not functional

--- bandwidthTest.py
+++ bandwidthTest.py
@@ -1,5 +1,6 @@
 import torch
 import torch.distributed as dist
+import torch.cuda as cuda
 # import argparse
 import logging
 import os
@@ -10,30 +11,36 @@ from utilsNersc.distributed import init_workers
 
 
 # max and base of exponents to modify data size
-maxExp = 8
+maxExp = 6
 base = 4
 
 def run(world_size, rank):
     
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    device = torch.device('cuda' if cuda.is_available() else 'cpu')
+    name = cuda.get_device_properties(device)
+    logging.info("Device name: %s", name)
+    dist.barrier()
+    logging.info("Sync time")
+    t0 = time.time()
     for i in range(0,rank):
         #recv data of all ranks before
         logging.info("Accepting from rank %i", i)
         for j in range(maxExp):
-            # logging.info("Rank %i Hit barrier 1, #%i", rank, j)
-            # dist.barrier()
-            # logging.info("Rank %i Passed barrier 1", rank)
+            logging.info("Rank %i Hit barrier", rank, )
+            dist.barrier()
+            logging.info("Rank %i Passed barrier", rank)
 
-            buffer = torch.zeros(250*(base**maxExp)).to(device)
+            buffer = torch.rand(250*(base**j)).to(device)
             dist.recv(tensor=buffer, src=i)
-            start  = time.time()
-            logging.info("recv %i -> %i # %i at %f", i, rank, j, start)
+            start  = time.time() - t0
+            logging.info("recv %i -> %i # %i at %f, %f %f", i, rank, j, start, buffer[0], buffer[1])
             
     logging.info("Sending from rank %i", rank)
     for j in range(maxExp):
-        # logging.info("Rank %i Hit barrier 2", rank)
-        # dist.barrier()
-        # logging.info("Rank %i Passed barrier 2", rank)
+        logging.info("Rank %i Hit barrier", rank)
+        dist.barrier()
+        logging.info("Rank %i Passed barrier", rank)
         for i in range(world_size):
             #send data to all ranks
             #ensure it does not send to itself
@@ -41,22 +48,135 @@ def run(world_size, rank):
             #generate random tensor of correct size
                 temp = torch.rand(250*(base**j)).to(device)
                 dist.send(tensor=temp,dst=i)
-                start  = time.time()
-                logging.info("sent %i -> %i # %i at %f", rank, i, j, start)
+                start  = time.time() - t0
+                logging.info("sent %i -> %i # %i at %f, %f %f", rank, i, j, start, temp[0], temp[1])
 
     for i in range(rank+1,world_size):
         #recv data of all ranks after
         logging.info("Accepting from rank %i", i)
         for j in range(maxExp):
-            # logging.info("Rank %i Hit barrier 3", rank)
-            # dist.barrier()
-            # logging.info("Rank %i Passed barrier 3", rank)
+            logging.info("Rank %i Hit barrier", rank)
+            dist.barrier()
+            logging.info("Rank %i Passed barrier", rank)
 
-            buffer = torch.zeros(250*(base**maxExp)).to(device)
+            buffer = torch.rand(250*(base**j)).to(device)
             dist.recv(tensor=buffer, src=i)
-            start = time.time()
-            logging.info("recv %i -> %i # %i at %f", i, rank, j, start)
+            start = time.time() - t0
+            logging.info("recv %i -> %i # %i at %f, %f %f", i, rank, j, start, buffer[0], buffer[1])
 
+    # dist.barrier()
+    # t0 = time.time()
+    # for i in range(5):
+    #     size = 1000**(i+1)
+    #     temp = torch.rand(size).to(device)
+    #     if rank == 0:
+    #         dist.barrier()
+    #         logging.info("Rank 0 Start send")
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=1)
+    #         now = time.time() - t0
+    #         logging.info("Sent 0 -> 1, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=2)
+    #         now = time.time() - t0
+    #         logging.info("Sent 0 -> 2, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=3)
+    #         now = time.time() - t0
+    #         logging.info("Sent 0 -> 3, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=1)
+    #         now = time.time() - t0
+    #         logging.info("Recv 1 -> 0, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=2)
+    #         now = time.time() - t0
+    #         logging.info("Recv 2 -> 0, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=3)
+    #         now = time.time() - t0
+    #         logging.info("Recv 3 -> 0, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #     elif rank == 1:
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=0)
+    #         now = time.time() - t0
+    #         logging.info("Recv 0 -> 1, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         logging.info("Rank 1 Start send")
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=0)
+    #         now = time.time() - t0
+    #         logging.info("Sent 1 -> 0, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=2)
+    #         now = time.time() - t0
+    #         logging.info("Sent 1 -> 2, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=3)
+    #         now = time.time() - t0
+    #         logging.info("Sent 1 -> 3, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=2)
+    #         now = time.time() - t0
+    #         logging.info("Recv 2 -> 1, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=3)
+    #         now = time.time() - t0
+    #         logging.info("Recv 3 -> 1, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #     elif rank == 2:
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=0)
+    #         now = time.time() - t0
+    #         logging.info("Recv 0 -> 2, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=1)
+    #         now = time.time() - t0
+    #         logging.info("Recv 1 -> 2, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         logging.info("Rank 2 Start send")
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=0)
+    #         now = time.time() - t0
+    #         logging.info("Sent 2 -> 0, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=1)
+    #         now = time.time() - t0
+    #         logging.info("Sent 2 -> 1, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=3)
+    #         now = time.time() - t0
+    #         logging.info("Sent 2 -> 3, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=3)
+    #         now = time.time() - t0
+    #         logging.info("Recv 3 -> 2, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #     elif rank == 3:
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=0)
+    #         now = time.time() - t0
+    #         logging.info("Recv 0 -> 3, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=1)
+    #         now = time.time() - t0
+    #         logging.info("Recv 1 -> 3, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         dist.recv(tensor=temp,src=2)
+    #         logging.info("Recv 2 -> 3, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         dist.barrier()
+    #         logging.info("Rank 3 Start send")
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=0)
+    #         now = time.time() - t0
+    #         logging.info("Sent 3 -> 0, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=1)
+    #         now = time.time() - t0
+    #         logging.info("Sent 3 -> 1, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    #         temp = torch.rand(size).to(device)
+    #         dist.send(tensor=temp,dst=2)
+    #         now = time.time() - t0
+    #         logging.info("Sent 3 -> 2, %f %f %f at %f", temp[0],temp[1],temp[2], now)
+    
     logging.info("Rank %i done", rank)    
     exit()
 
