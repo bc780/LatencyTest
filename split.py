2eeff4e2afdcd344a156e4cb910b281411dd7275
BRANDON CHAN <bc780@scarletmail.rutgers.edu>
2024-09-05 16:16:51 -0400

    updated shell script

--- split.py
+++ split.py
@@ -10,6 +10,6 @@ def split_file_by_breaker(input_file, breaker):
     
     print(f"File split into {len(parts)} parts.")
 
-input_file = 'slurm-30104147.out'
+input_file = 'slurm-30138608.out'
 breaker = 'BREAK'
 split_file_by_breaker(input_file, breaker)
